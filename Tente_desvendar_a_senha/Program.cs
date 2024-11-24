0a60af4bad68529e095b39adf04772e370722f5e
JuanPedroCMP <juanpedrocmp.15@gmail.com>
2024-11-24 19:24:58 -0300

    Adicionar arquivos de projeto.

---
+++ Tente_desvendar_a_senha/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Tente_desvendar_a_senha
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
