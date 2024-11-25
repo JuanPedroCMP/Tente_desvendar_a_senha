35c21e7c353a761c8637f9922e1281a54edd511f
JuanPedroCMP <juanpedrocmp.15@gmail.com>
2024-11-24 21:52:37 -0300

    Desenvolvimento de uma parte do algorítimo

--- Tente_desvendar_a_senha/Program.cs
+++ Tente_desvendar_a_senha/Program.cs
@@ -1,10 +1,33 @@
-﻿namespace Tente_desvendar_a_senha
+﻿using static System.Console;
+
+namespace Tente_desvendar_a_senha
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            WriteLine("Seja bem vindo!");
+            Thread.Sleep(500);
+            Clear();
+            Write("\n\n\n");
+            Write(@"    ▄▀█ █▀▄ █ █░█ █ █▄░█ █░█ █▀▀   ▄▀█   █▀ █▀▀ █▄░█ █░█ ▄▀█
+    █▀█ █▄▀ █ ▀▄▀ █ █░▀█ █▀█ ██▄   █▀█   ▄█ ██▄ █░▀█ █▀█ █▀█");
+            Thread.Sleep(150);
+            Write("\n\t\t Precione qualque tecla para continuar...");
+            ReadKey(true);
+            Clear();
+            Write(@"
+    ╔═════════════════════════════════╗
+    ║           DIFICULDADE           ║
+    ╠═════════════════════════════════╣
+    ║        [1] Fácil (QD = 4)       ║
+    ║        [2] Médio (QD = 6)       ║
+    ║        [3] Dificíl (QD = 10)    ║
+    ╚═════════════════════════════════╝
+    QD = Quandidade de dígitos
+");
+
+
         }
     }
 }
